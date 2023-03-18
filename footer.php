3372cac521c74d3bb583a1acd901e603c78f5b41
yamahiro01 <70377815+yamahiro01@users.noreply.github.com>
2023-03-18 23:54:20 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!-- jQueryの読み込み -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.6.1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+
+<?php wp_footer(); ?>
